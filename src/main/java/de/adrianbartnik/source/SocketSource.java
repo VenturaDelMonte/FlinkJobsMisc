f3e5c8d1efb2ec1ab37a521cb2795aaddad31038
Adrian Bartnik <git@adrianbartnik.de>
2017-12-08 13:52:35 +0100

    Added KafkaSource

--- src/main/java/de/adrianbartnik/source/SocketSource.java
+++ src/main/java/de/adrianbartnik/source/SocketSource.java
@@ -5,6 +5,9 @@ import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
 public class SocketSource extends AbstractSource<String> {
+
+    private static final String OPERATOR_NAME = "SocketSource";
+
     @Override
     public DataStream<String> createSource(String[] arguments, StreamExecutionEnvironment executionEnvironment) {
         // the host and the port to connect to
@@ -13,6 +16,6 @@ public class SocketSource extends AbstractSource<String> {
         final int port = params.getInt("port", 9000);
         final String delimiter = params.get("delimiter", "\n");
 
-        return executionEnvironment.socketTextStream(hostname, port, delimiter);
+        return executionEnvironment.socketTextStream(hostname, port, delimiter).name(OPERATOR_NAME);
     }
 }

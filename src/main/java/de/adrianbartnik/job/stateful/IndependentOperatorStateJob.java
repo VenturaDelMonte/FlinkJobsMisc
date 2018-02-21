18064c10b27acaf748065b687cab543747e970a1
Adrian Bartnik <git@adrianbartnik.de>
2018-02-21 12:27:01 +0100

    Fixed error in OperatorStateJob with new StateBackend

--- src/main/java/de/adrianbartnik/job/stateful/IndependentOperatorStateJob.java
+++ src/main/java/de/adrianbartnik/job/stateful/IndependentOperatorStateJob.java
@@ -5,6 +5,9 @@ import de.adrianbartnik.operator.CountingMap;
 import de.adrianbartnik.sink.TextOutputSink;
 import de.adrianbartnik.source.IntervalSequenceSource;
 import org.apache.flink.api.java.utils.ParameterTool;
+import org.apache.flink.runtime.state.filesystem.FsStateBackend;
+import org.apache.flink.runtime.state.memory.MemoryStateBackend;
+import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
 public class IndependentOperatorStateJob {
@@ -19,14 +22,30 @@ public class IndependentOperatorStateJob {
         final int sourceParallelism = params.getInt("sourceParallelism", 3);
         final int mapParallelism = params.getInt("mapParallelism", 4);
         final int sinkParallelism = params.getInt("sinkParallelism", 2);
+        final String external_backend = params.get("backend", "memory");
+        final String file_system_backend_path = params.get("fsBackendPath", "file://tmp/");
+        final boolean asynchronousCheckpoints = params.getBoolean("asynchronousCheckpoints", true);
 
-        FlinkJobFactory<Long, String> creator = new FlinkJobFactory<>(args, false, true);
+        StreamExecutionEnvironment env = new FlinkJobFactory<>(args, false, true).setupExecutionEnvironment();
 
-        StreamExecutionEnvironment job =
-                creator.createJob(new IntervalSequenceSource(0, maxNumberOfMessages, pauseDuration, sourceParallelism),
-                        new CountingMap<>(mapParallelism),
-                        new TextOutputSink<>(sinkParallelism));
+        switch (external_backend) {
+            case "memory":
+                env.setStateBackend(new MemoryStateBackend());
+                break;
+            case "filesystem":
+                env.setStateBackend(new FsStateBackend(file_system_backend_path, asynchronousCheckpoints));
+                break;
+            default:
+                throw new IllegalArgumentException();
+        }
 
-        job.execute(JOB_NAME);
+        DataStream<Long> source = new IntervalSequenceSource(0, maxNumberOfMessages, pauseDuration, sourceParallelism)
+                .createSource(args, env);
+
+        DataStream<String> coutingMap = new CountingMap<Long>(mapParallelism).createOperator(args, source);
+
+        new TextOutputSink<String>(sinkParallelism).createSink(args, coutingMap);
+
+        env.execute(JOB_NAME);
     }
 }
